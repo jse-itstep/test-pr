62cee401b0bef25806592ee54ad4dd262ae8d5e0
Еламан Сураншиев <@itstep.loc>
2022-11-15 21:29:40 +0600

    file created

---
+++ yelaman.js
@@ -0,0 +1,11 @@
+let array = (arr) => {
+    let putNum = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            putNum.push(arr[i])
+        }
+        return putNum
+    }
+}
+let arr = [1,2,3,4,5,6,7,8,9]
+array(arr);
