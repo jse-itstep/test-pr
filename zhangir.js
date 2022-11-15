e6a6f8bcbcbc6edea1a049b741b45eb325eb2f3a
Жангир Уакасов <zangiruakasov@gmail.com>
2022-11-15 21:39:57 +0600

    new file Zhangir

--- zhangir.js
+++ zhangir.js
@@ -4,8 +4,8 @@ const deleteOddFromArray = (arr) => {
         if(arr[i] % 2 === 0){
             onlyEven.push(arr[i])
         }
-        return onlyEven
     }
+    return onlyEven
 }
 
 let arr = [1,2,3,4,5,6,7,8,9]
